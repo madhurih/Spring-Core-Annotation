eef824ebc60df0c8f00eef142f68a9a32a068b34
madhurih <hiremadhuri66@gmail.com>
2019-05-27 14:00:09 +0530

    Task 2 completed

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -21,20 +21,26 @@ public class Main
         ctx.refresh();
 
         Movie bean = (Movie) ctx.getBean("Movie.class");
-        System.out.println("Movie ID: "+bean.getId());
-        System.out.println("Movie Name: "+bean.getMovieName());
-        System.out.println("Actor Name: "+bean.getActor().getName());
-        System.out.println("Actor Age: "+bean.getActor().getAge());
-        System.out.println("Actor Gender: "+bean.getActor().getGender());
+//        System.out.println("Movie ID: "+bean.getId());
+//        System.out.println("Movie Name: "+bean.getMovieName());
+//        System.out.println("Actor Name: "+bean.getActor().getName());
+//        System.out.println("Actor Age: "+bean.getActor().getAge());
+//        System.out.println("Actor Gender: "+bean.getActor().getGender());
 
-        //System.out.println(bean.toString());
+        System.out.println(bean.toString());
         System.out.println("");
-        Movie bean1 = (Movie) ctx.getBean("Move.class");
-        System.out.println("Movie ID: "+bean1.getId());
-        System.out.println("Movie Name: "+bean1.getMovieName());
-        System.out.println("Actor Name: "+bean1.getActor().getName());
-        System.out.println("Actor Age: "+bean1.getActor().getAge());
-        System.out.println("Actor Gender: "+bean1.getActor().getGender());
 
+        Movie bean1 = (Movie) ctx.getBean("Move.class");
+//        System.out.println("Movie ID: "+bean1.getId());
+//        System.out.println("Movie Name: "+bean1.getMovieName());
+//        System.out.println("Actor Name: "+bean1.getActor().getName());
+//        System.out.println("Actor Age: "+bean1.getActor().getAge());
+//        System.out.println("Actor Gender: "+bean1.getActor().getGender());
+        System.out.println(bean1.toString());
+
+        //check scope of bean
+        Movie movie1 = (Movie) ctx.getBean("Movie.class");
+        Movie movie2 = (Movie) ctx.getBean("Movie.class");
+        System.out.println(movie1 == movie2);
     }
 }
--- src/main/java/com/stackroute/config/SpringBeanConfiguration.java
+++ src/main/java/com/stackroute/config/SpringBeanConfiguration.java
@@ -4,42 +4,44 @@ import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class SpringBeanConfiguration
 {
     //configure any bean here
     @Bean(name = "Movie.class")
+    @Scope("prototype")
     public Movie getMovie()
     {
-        Actor actor=new Actor();
-        actor.setName("Madhuri");
-        actor.setAge(30);
-        actor.setGender("F");
-
-        Movie movie=new Movie(actor);
-        movie.setMovieName("kalank");
-        movie.setId(01);
-        movie.setActor(actor);
-//        Actor actor = new Actor("Madhuri", "F", 30);
-//        Movie movie = new Movie(1, "kalank", actor);
+//        Actor actor=new Actor();
+//        actor.setName("Madhuri");
+//        actor.setAge(30);
+//        actor.setGender("F");
+//
+//        Movie movie=new Movie(actor);
+//        movie.setMovieName("kalank");
+//        movie.setId(01);
+//        movie.setActor(actor);
+        Actor actor = new Actor("Madhuri", "F", 30);
+        Movie movie = new Movie(1, "kalank", actor);
         return movie;
     }
 
     @Bean(name = "Move.class")
     public Movie getMove()
     {
-        Actor actor=new Actor();
-        actor.setName("varun");
-        actor.setAge(30);
-        actor.setGender("M");
-
-        Movie movie=new Movie(actor);
-        movie.setMovieName("Student of the year");
-        movie.setId(02);
-        movie.setActor(actor);
-//        Actor actor = new Actor("Madhuri", "F", 30);
-//        Movie movie = new Movie(1, "kalank", actor);
+//        Actor actor=new Actor();
+//        actor.setName("varun");
+//        actor.setAge(30);
+//        actor.setGender("M");
+//
+//        Movie movie=new Movie(actor);
+//        movie.setMovieName("Student of the year");
+//        movie.setId(02);
+//        movie.setActor(actor);
+        Actor actor = new Actor("Madhuri", "F", 30);
+        Movie movie = new Movie(1, "kalank", actor);
         return movie;
     }
 }
